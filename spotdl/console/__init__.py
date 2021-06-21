a90a49fd7da4aedab3fed7ed2b82593463961156
Jakub Kot <42355410+xnetcat@users.noreply.github.com>
2021-06-21 23:55:44 +0200

    moved most of the code to separate file

---
+++ spotdl/console/__init__.py
@@ -0,0 +1,79 @@
+import os
+import sys
+import signal
+import pkg_resources
+
+from spotdl.download import ffmpeg
+from spotdl.parsers import parse_arguments
+from spotdl.parsers.query_parser import parse_query
+from spotdl.search.spotifyClient import SpotifyClient
+from spotdl.download.downloader import DownloadManager
+
+
+def console_entry_point():
+    """
+    This is where all the console processing magic happens.
+    Its super simple, rudimentary even but, it's dead simple & it works.
+    """
+
+    # If -v parameter is specified print version and exit
+    if len(sys.argv) >= 2 and sys.argv[1] in ["-v", "--version"]:
+        version = pkg_resources.require("spotdl")[0].version
+        print(version)
+        sys.exit(0)
+
+    # Parser arguments
+    arguments = parse_arguments()
+
+    # Convert arguments to dict
+    args_dict = vars(arguments)
+
+    # Check if ffmpeg has correct version, if not exit
+    if (
+        ffmpeg.has_correct_version(
+            arguments.ignore_ffmpeg_version, arguments.ffmpeg or "ffmpeg"
+        )
+        is False
+    ):
+        sys.exit(1)
+
+    # Initialize spotify client
+    SpotifyClient.init(
+        client_id="5f573c9620494bae87890c0f08a60293",
+        client_secret="212476d9b0f3472eaa762d90b19b0ba8",
+        user_auth=arguments.user_auth,
+    )
+
+    # Change directory if user specified correct output path
+    if arguments.output:
+        if not os.path.isdir(arguments.path):
+            sys.exit("The output directory doesn't exist.")
+        print(f"Will download to: {os.path.abspath(arguments.output)}")
+        os.chdir(arguments.path)
+
+    # Start download manager
+    with DownloadManager(args_dict) as downloader:
+        if not arguments.debug_termination:
+            def graceful_exit(signal, frame):
+                downloader.display_manager.close()
+                sys.exit(0)
+
+            signal.signal(signal.SIGINT, graceful_exit)
+            signal.signal(signal.SIGTERM, graceful_exit)
+
+        # Find tracking files in queries
+        tracking_files = [
+            query for query in arguments.query if query.endswith(".spotdlTrackingFile")
+        ]
+
+        # Restart downloads
+        for tracking_file in tracking_files:
+            print("Preparing to resume download...")
+            downloader.resume_download_from_tracking_file(tracking_file)
+
+        # Get songs
+        song_list = parse_query(arguments.query, arguments.format)
+
+        # Start downloading
+        if len(song_list) > 0:
+            downloader.download_multiple_songs(song_list)
