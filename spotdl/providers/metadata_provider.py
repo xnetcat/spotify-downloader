2a15826794c316a3991c94db5b285a34b121d657
Jakub Kot <xnetcat.dev@gmail.com>
2021-07-18 18:21:07 +0200

    removed get_song_lyrics function, as it's already in the provider utils

--- spotdl/providers/metadata_provider.py
+++ spotdl/providers/metadata_provider.py
@@ -22,37 +22,3 @@ def from_url(spotify_url: str):
     raw_album_meta = spotify_client.album(albumId)
 
     return raw_track_meta, raw_artist_meta, raw_album_meta
-
-
-def get_song_lyrics(song_name: str, song_artists: List[str]) -> str:
-    """
-    Gets the metadata of the song.
-
-    `str` `song_name` : name of song
-    `list<str>` `song_artists` : list containing name of contributing artists
-
-    returns `str` : Lyrics of the song.
-    """
-
-    headers = {
-        "Authorization": "Bearer alXXDbPZtK1m2RrZ8I4k2Hn8Ahsd0Gh_o076HYvcdlBvmc0ULL1H8Z8xRlew5qaG",
-    }
-    api_search_url = "https://api.genius.com/search"
-    search_query = f'{song_name} {", ".join(song_artists)}'
-
-    api_response = requests.get(
-        api_search_url, params={"q": search_query}, headers=headers
-    ).json()
-
-    song_id = api_response["response"]["hits"][0]["result"]["id"]
-    song_api_url = f"https://api.genius.com/songs/{song_id}"
-
-    api_response = requests.get(song_api_url, headers=headers).json()
-
-    song_url = api_response["response"]["song"]["url"]
-
-    genius_page = requests.get(song_url)
-    soup = BeautifulSoup(genius_page.text, "html.parser")
-    lyrics = soup.select_one("div.lyrics").get_text()
-
-    return lyrics.strip()
